2cdce4f80a28690f36e369620cc5063c1422a67b
Kage-Hermit <gerald.direxx@gmail.com>
2019-09-01 23:16:47 +0100

    clean commit

---
+++ toggle-bar.js
@@ -0,0 +1,23 @@
+if($('.owl-carousel').length > 0){
+    $('#bestSellerCarousel').owlCarousel({
+      loop:true,
+      margin:30,
+      nav:true,
+      navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
+      dots: false,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items: 2
+        },
+        900:{
+          items:3
+        },
+        1130:{
+          items:4
+        }
+      }
+    })
+  }
